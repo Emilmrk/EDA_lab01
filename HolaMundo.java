becbf2fb7dc652158a3c23193da88b66ec664210
Emilmrk <93278758+Emilmrk@users.noreply.github.com>
2023-05-26 14:39:33 -0500

    Update HolaMundo.java
    "Mi nombre es:"

--- HolaMundo.java
+++ HolaMundo.java
@@ -1,5 +1,8 @@
 public class HolaMundo
+  import java.util,*;
+Static Scanner sc  = new Scanner(System.in);
 public static void main(String args[]){
   System.out.println ("¡Hola mundo!");
   System.out.println ("Bienvenida al curso");
+   System.out.println ("ingrese su nombre por favor: \n"+sc.next);
 }
